93cd0c0d1601fedf4dc73fd207e2b01977181308
Carlos <cfernande1408@gmail.com>
2022-10-25 18:21:12 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlmacenEmpleados<Director> empleados = new AlmacenEmpleados<Director>(3);
+            empleados.agregar(new Director(4500));
+        
+        }
+
+        class AlmacenEmpleados<T> where T: IParaEmpleados
+        {
+
+            public AlmacenEmpleados(int z)
+            {
+                datosEmpleado = new T[z];
+            }
+            
+            public void agregar( T obj)
+            {
+
+                datosEmpleado[i] = obj;
+
+                i++;
+
+            }
+
+            public T getEmpleado(int i)
+            {
+                return datosEmpleado[i];
+            }
+
+            private int i = 0;
+
+            private T[] datosEmpleado;
+
+        }
+
+        class Secretaria : IParaEmpleados
+        {
+            public Secretaria(double salario)
+            {
+                this.salario = salario;
+
+            }
+
+            private double salario;
+
+            public double getSalario()
+            {
+
+                return this.salario;
+            }
+        }
+
+        class Electricista
+        {
+            public Electricista(double salario)
+            {
+                this.salario = salario;
+
+            }
+
+            private double salario;
+
+            public double getSalario()
+            {
+
+                return this.salario;
+            }
+
+        }
+
+        class Director : IParaEmpleados
+        {
+
+            public Director(double salario)
+            {
+                this.salario = salario;
+
+            }
+
+            private double salario;
+
+            public double getSalario()
+            {
+
+                return this.salario;
+
+            }
+        }
+
+        interface IParaEmpleados
+        {
+            double getSalario();
+        }
+    }
+}
